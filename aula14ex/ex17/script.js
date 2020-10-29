4b6cf088a60d90bd6f2aafa9e1094a928f262be3
Guilherme Araujo <guilherme.alm.araujo@gmail.com>
2020-10-29 15:27:15 -0300

    Inclusão do exercício 17 da aula 14

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -1,1 +1,33 @@
-alert('Olá')
+function gerartabuada(){
+
+    let num = document.getElementById('numtxt')
+
+    let text = document.getElementById('seltab')
+    
+
+    if(num.value.length == 0){
+        
+        window.alert('Por favor, digite um número!')
+
+    }else{
+        
+        let numTab = Number(num.value)
+
+        let c = 1
+
+        text.innerHTML = ''
+
+        while(c <= 10){
+            let item = document.createElement('option')
+            item.text = `${c} x ${num.value} = ${c*num.value}`
+            item.value = `tab${c}`
+            text.appendChild(item)
+            c++
+        }
+
+    }
+
+    
+
+
+}
