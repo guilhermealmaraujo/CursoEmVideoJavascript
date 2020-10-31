2b1887923395eb7f9b572fa2d075c0cdd0494f7b
Guilherme Araujo <guilherme.alm.araujo@gmail.com>
2020-10-30 21:12:48 -0300

    Incluindo exemplo da aula 17, final

---
+++ aula17/objeto01.js
@@ -0,0 +1,14 @@
+let amigo = {nome:'José', 
+sexo: 'M', 
+peso: 85.4,
+engordar(p){
+    console.log('Engordou!')
+    this.peso += p
+}}
+
+console.log(amigo)
+
+amigo.engordar(2)
+
+console.log(`${amigo.nome} pesa ${amigo.peso} Kg`)
+
